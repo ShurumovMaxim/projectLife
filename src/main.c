342b62baf2a4d86847b0c42010c4437d19ae923c
Или Даниил Анатольевич <ip512s06@awp06.csc.local>
2016-05-30 15:59:07 +0600

    SKELETON

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define CTEST_MAIN
+
+// uncomment lines below to enable/disable features. See README.md for details
+#define CTEST_SEGFAULT
+//#define CTEST_NO_COLORS
+//#define CTEST_COLOR_OK
+
+#include "ctest.h"
+#include "bibl.h"
+
+int main()
+{
+    float a, b, c;
+    printf("vvedite a: "); scanf("%f", &a);
+    printf ("vvedite b: "); scanf("%f", &b);
+    printf("vvedite c: "); scanf("%f", &c);
+    func1(a, b, c);
+    return 1;
+    
+}
+
+
---
+++ thirdparty/bibl.h
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int state;
+    float res1;
+    float res2;
+} Square;
+
+Square func1(float a, float b, float c);
+
+
+
+
+
