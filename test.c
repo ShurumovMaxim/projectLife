677779b4a2dec3cd8d9911654089891693c3088f
Или Даниил Анатольевич <ip512s06@awp06.csc.local>
2016-05-30 12:48:54 +0600

    tratatat

--- test.c
+++ test.c
@@ -9,3 +9,10 @@ CTEST(distance_suite, simple_test) {
     ASSERT_EQUAL(10, z.res2);
 }
 
+CTEST(distance_suite, simple_test) {
+    float a=1, b=7, c=6;
+    Square z = func1(a,b,c);
+    ASSERT_EQUAL(-1, z.res1);
+    ASSERT_EQUAL(-6, z.res2);
+}
+
